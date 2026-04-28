584c4c02252ab42bf16252ac8cd498b513b983a1
anshika13raj <154364972+anshika13raj@users.noreply.github.com>
2026-04-28 21:03:34 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,35 +1,16 @@
 
 
-// const API = "https://gatepass-backend-22si.onrender.com";
-
-// export const getUsers = () =>
-//   fetch(`${API}/api/users`).then((r) => r.json());
-
-// export const getLogs = () =>
-//   fetch(`${API}/api/logs`).then((r) => r.json());
-
-// export const addUser = (data) =>
-//   fetch(`${API}/api/addUser`, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(data),
-//   });
-
-// export const approveUser = (cardID) =>
-//   fetch(`${API}/api/approve`, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ cardID }),
-//   });
-
-// export const loginAdmin = (data) =>
-//   fetch(`${API}/api/login`, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(data),
-//   });
-
 const API = "https://gatepass-backend-22si.onrender.com";
+
+export const deleteLog = (index) =>
+  fetch(`${API}/api/log/${index}`, {
+    method: "DELETE",
+  });
+
+export const getLogsFiltered = (search = "", type = "All") =>
+  fetch(
+    `${API}/api/logs?search=${search}&type=${type}`
+  ).then((r) => r.json());
 
 export const getUsers = () =>
   fetch(`${API}/api/users`).then((r) => r.json());
