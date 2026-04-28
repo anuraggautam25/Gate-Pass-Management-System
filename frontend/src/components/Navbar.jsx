3ce43b2bb288a6c0aed1ddb575337dc4c720e623
anshika13raj <154364972+anshika13raj@users.noreply.github.com>
2026-04-28 22:07:43 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
+// function Navbar() {
+//   return (
+//     <div className="navbar">
+//       <h2>Gate Pass System</h2>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+import gailLogo from "../assets/gail.png";
+
 function Navbar() {
   return (
     <div className="navbar">
-      <h2>Gate Pass System</h2>
+      <div className="nav-left">
+        <img src={gailLogo} alt="GAIL" className="nav-logo" />
+        <div className="nav-title">GAIL Gate Pass System</div>
+      </div>
+
+      <div>
+        {/* optional right side */}
+      </div>
     </div>
   );
 }
