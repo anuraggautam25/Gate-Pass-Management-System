b4478ff2512b016c47dae37df826e15ee6193c75
anshika13raj <154364972+anshika13raj@users.noreply.github.com>
2026-04-28 22:10:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,26 @@
+// import { useState } from "react";
+// import Login from "./pages/Login";
+// import Dashboard from "./pages/Dashboard";
+// import Admin from "./pages/Admin";
+// import Navbar from "./components/Navbar";
+
+
+// function App() {
+//   const [login, setLogin] = useState(false);
+
+//   if (!login) return <Login setLogin={setLogin} />;
+
+//   return (
+//     <div>
+//       <Navbar />
+//       <Dashboard />
+//       <Admin />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { useState } from "react";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -6,15 +29,19 @@ import Navbar from "./components/Navbar";
 
 function App() {
   const [login, setLogin] = useState(false);
+  const [page, setPage] = useState("dashboard"); // simple navigation
 
+  // Login screen
   if (!login) return <Login setLogin={setLogin} />;
 
   return (
-    <div>
-      <Navbar />
-      <Dashboard />
-      <Admin />
-    </div>
+    <>
+      <Navbar setPage={setPage} />
+
+      {/* PAGE SWITCHING */}
+      {page === "dashboard" && <Dashboard />}
+      {page === "admin" && <Admin />}
+    </>
   );
 }
 
